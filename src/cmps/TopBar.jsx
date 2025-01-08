f58a832ede5eb2feb5fce7598488b19509968bce
alonWohl <alonw1010@gmail.com>
2025-01-08 14:53:02 +0200

    Add top-bar header

--- src/cmps/TopBar.jsx
+++ src/cmps/TopBar.jsx
@@ -1,27 +1,38 @@
+import { useSelector } from 'react-redux'
 import { svgs } from '../services/svg.service'
+import { Link } from 'react-router-dom'
 
 export function TopBar() {
+	const DEFAULT_AVATAR = ' https://res.cloudinary.com/dqfhbqcwv/image/upload/v1736337067/dapulse_default_photo_kvhsyn.png'
+	const user = useSelector(storeState => storeState.userModule.user)
+
 	return (
 		<div className="top-bar flex ">
 			<header className="top-bar-header flex align-center justify-between full">
 				<div className="switcher-logo flex align-center">
-					<div className="switcher">{svgs.dottedMenuBtn}</div>
-					<div className="logo">{svgs.mgmtLogo}</div>
-					<div className="product">
-						<span>monday</span>
-						<span>work managment</span>
-					</div>
+					<button className="switcher flex align-center">{svgs.dottedMenuBtn}</button>
+
+					<Link to="/" className="logo-product flex clean-link">
+						<div className="logo flex align-center">{svgs.mgmtLogo}</div>
+						<h1 className="product flex align-center">
+							<span className="mundane">mundane</span>
+						</h1>
+					</Link>
 				</div>
 				<div></div>
-				<nav className="top-bar-nav flex">
-					<div className="notification">{svgs.bell}</div>
-					<div className="invite">{svgs.addMember}</div>
-					<div className="search">{svgs.search}</div>
-					<div className="avatar-with-company-logo">
-						<div className="account-logo-wrapper flex">
-							<img style={{ width: '24px', height: '24px', borderRadius: '4px' }} src="https://cdn.monday.com/images/logos/monday_logo_icon.png" alt="logo" />
-						</div>
-					</div>
+				<nav className="top-bar-nav flex align-center">
+					<button className="nav-btn flex justify-center align-center">{svgs.bell}</button>
+					<button className="nav-btn flex justify-center align-center">{svgs.addMember}</button>
+					<button className="nav-btn flex justify-center align-center">{svgs.search}</button>
+
+					<button className="avatar-with-company-logo flex justify-center align-center">
+						<Link className="flex" to={`/user/${user?._id}`}>
+							<div className="account-logo-wrapper flex justify-center align-center">
+								<img style={{ width: '24px', height: '24px', borderRadius: '4px' }} src="https://cdn.monday.com/images/logos/monday_logo_icon.png" alt="logo" />
+							</div>
+							<img className="avatar-image" src={user?.imgUrl || DEFAULT_AVATAR} alt="avatar" />
+						</Link>
+					</button>
 				</nav>
 			</header>
 		</div>
