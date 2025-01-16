bed593c322f4524bc561ee042dccface5da6cf25
alonWohl <alonw1010@gmail.com>
2025-01-16 19:52:32 +0200

    Remove unused line

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -23,27 +23,26 @@ export function RootCmp() {
 	return (
 		<div className="main-container">
 			<TopBar />
-			{/* <AppHeader /> */}
-            <SideBar />
+			<SideBar />
 			<UserMsg />
 
-            <main>
-                <Routes>
-                    <Route path="" element={<HomePage />} />
-                    <Route path="about" element={<AboutUs />} />
-                    <Route path="dashboard" element={<Dashboard />} />
-                    <Route path="board" element={<BoardIndex />} />
-                    <Route path="board/:boardId" element={<BoardDetails />}>
-                    	<Route path="task/:taskId" element={<TaskDetails />} />
+			<main>
+				<Routes>
+					<Route path="" element={<HomePage />} />
+					<Route path="about" element={<AboutUs />} />
+					<Route path="dashboard" element={<Dashboard />} />
+					<Route path="board" element={<BoardIndex />} />
+					<Route path="board/:boardId" element={<BoardDetails />}>
+						<Route path="task/:taskId" element={<TaskDetails />} />
 					</Route>
-                    <Route path="user/:id" element={<UserDetails />} />
-                    <Route path="admin" element={<AdminIndex />} />
-                    <Route path="login" element={<LoginSignup />}>
-                        <Route index element={<Login />} />
-                        <Route path="signup" element={<Signup />} />
-                    </Route>
-                </Routes>
-            </main>
-        </div>
-    )
+					<Route path="user/:id" element={<UserDetails />} />
+					<Route path="admin" element={<AdminIndex />} />
+					<Route path="login" element={<LoginSignup />}>
+						<Route index element={<Login />} />
+						<Route path="signup" element={<Signup />} />
+					</Route>
+				</Routes>
+			</main>
+		</div>
+	)
 }
