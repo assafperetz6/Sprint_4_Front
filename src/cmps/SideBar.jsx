d9ccef9632a587f3334a8afc3a7b094f4e24d027
assafperetz6 <assafperetz6@gmail.com>
2025-01-26 10:33:17 +0200

    Add seperate btn for collapsed menu

--- src/cmps/SideBar.jsx
+++ src/cmps/SideBar.jsx
@@ -4,20 +4,25 @@ import { NavLink, useLocation } from 'react-router-dom'
 
 import { svgs } from '../services/svg.service'
 import { boardService } from '../services/board'
-import { loadBoards, addBoard, removeBoard, updateBoard } from '../store/actions/board.actions'
+import {
+	loadBoards,
+	addBoard,
+	removeBoard,
+	updateBoard,
+} from '../store/actions/board.actions'
 
 import { ContextMenu } from '../cmps/ContextMenu.jsx'
 import { InlineEdit } from './InlineEdit.jsx'
 
 export function SideBar() {
-	const boards = useSelector(storeState => storeState.boardModule.boards)
-	const loggedInUser = useSelector(storeState => storeState.userModule.user)
+	const boards = useSelector((storeState) => storeState.boardModule.boards)
+	const loggedInUser = useSelector((storeState) => storeState.userModule.user)
 
 	const [activeMenuId, setActiveMenuId] = useState(null)
 	const [boardToEdit, setboardToEdit] = useState(null)
 	const [isCollapsed, setIsCollapsed] = useState(false)
 	const [isEditing, setIsEditing] = useState(true)
-	const [isHovered, setisHovered] = useState(false)
+	const [isHovered, setIsHovered] = useState(false)
 	const { pathname } = useLocation()
 	const toggleSidebarRef = useRef()
 	const buttonRefs = useRef({})
@@ -36,16 +41,16 @@ export function SideBar() {
 	}
 
 	function toggleSidebar() {
-		if (!isCollapsed) setisHovered(false)
-		setIsCollapsed(prev => !prev)
+		if (!isCollapsed) setIsHovered(false)
+		setIsCollapsed((prev) => !prev)
 	}
 
-	function handleMouseHover(ev) {
-		ev._reactName === 'onMouseEnter' && ev.target !== toggleSidebarRef.current ? setisHovered(true) : setisHovered(false)
-	}
+	// function handleMouseHover(ev) {
+	// 	ev._reactName === 'onMouseEnter' && ev.target !== toggleSidebarRef.current ? setisHovered(true) : setisHovered(false)
+	// }
 
 	function toggleContextMenu(ev, boardId) {
-		setActiveMenuId(prev => (prev === boardId ? null : boardId))
+		setActiveMenuId((prev) => (prev === boardId ? null : boardId))
 	}
 
 	function onRemoveBoard(boardId) {
@@ -73,72 +78,109 @@ export function SideBar() {
 
 	if (pathname.startsWith('/login')) return null
 	return (
-		<aside className={`sidebar ${isCollapsed ? 'collapsed' : ''} ${isHovered ? 'hovered' : ''}`} onMouseEnter={handleMouseHover} onMouseLeave={handleMouseHover}>
-			<button ref={toggleSidebarRef} onMouseLeave={handleMouseHover} className={`toggle-sidebar ${!isHovered ? 'hidden' : ''}`} onClick={toggleSidebar}>
+		<>
+			<button
+				ref={toggleSidebarRef}
+				className={`collapsed-toggle-sidebar ${!isCollapsed || isHovered ? 'hidden' : ''}`}
+				onClick={toggleSidebar}
+			>
 				{isCollapsed ? svgs.arrowRight : svgs.arrowLeft}
 			</button>
-			<nav>
-				<div>
-					<NavLink end to="/home">
-						{svgs.house} Home
-					</NavLink>
-				</div>
-				<NavLink to="/board">{svgs.myWork} My work</NavLink>
-			</nav>
-
-			<div className="favorite-container">
-				<button className="toggle-favorites">{svgs.star} Favorites</button>
-			</div>
-
-			<section className="workspaces-actions">
-				<div className="workspaces-wrapper">
-					<div className="workspaces-wrapper">
-						<div>{svgs.workspaces} Workspaces</div>
-						<button>{svgs.threeDots}</button>
-					</div>
-					<button>{svgs.search}</button>
-				</div>
-
-				<div className="workspaces-wrapper">
-					<button className="workspace-list-btn">
-						<div>S {svgs.workspaceHouse}</div>
-						<span>{getUserFirstName()}'s main workspace</span>
-					</button>
-					<button className="add-workspace-item" onClick={onAddBoard}>
-						{svgs.plus}
-					</button>
-				</div>
-			</section>
-
-			<section className="board-links">
-				{boards.map(board => (
-					<div key={board._id} className="link-wrapper">
-						<NavLink className="board-link" to={`/board/${board._id}`}>
-							{svgs.board}
-							{isEditing && boardToEdit === board._id ? (
-								<InlineEdit
-									value={board.title}
-									onSave={newTitle => {
-										onUpdateBoard({ ...board, title: newTitle })
-										setIsEditing(false)
-										setboardToEdit(null)
-									}}
-									isEditing={true}
-								/>
-							) : (
-								<span>{board.title}</span>
-							)}
+			<aside
+				className={`sidebar ${isCollapsed ? 'collapsed' : ''} ${
+					isHovered ? 'hovered' : ''
+				}`}
+				onMouseEnter={() => setIsHovered(true)}
+				onMouseLeave={() => setIsHovered(false)}
+			>
+				<button
+					ref={toggleSidebarRef}
+					className={`toggle-sidebar ${!isHovered ? 'hidden' : ''}`}
+					onClick={toggleSidebar}
+				>
+					{isCollapsed ? svgs.arrowRight : svgs.arrowLeft}
+				</button>
+				<nav>
+					<div>
+						<NavLink end to="/home">
+							{svgs.house} Home
 						</NavLink>
-						<button className={`board-options ${activeMenuId === board._id ? 'open' : ''}`} onClick={ev => toggleContextMenu(ev, board._id)} ref={el => (buttonRefs.current[board._id] = el)}>
-							{svgs.threeDots}
-						</button>
-
-						{/* {activeMenuId === board._id && <ContextMenu board={board} onClose={() => setActiveMenuId(null)} onRemoveBoard={onRemoveBoard} onUpdateBoard={onUpdateBoard} onRenameBoard={onRenameBoard} />} */}
-						{activeMenuId === board._id && <ContextMenu type="board" entity={board} onClose={() => setActiveMenuId(null)} onRemove={onRemoveBoard} onUpdate={onUpdateBoard} onRename={onRenameBoard} referenceElement={buttonRefs.current[board._id]} />}
 					</div>
-				))}
-				<NavLink to="/dashboard">{svgs.dashboard} Dashboard and reporting</NavLink>
-			</section>
-		</aside>
+					<NavLink to="/board">{svgs.myWork} My work</NavLink>
+				</nav>
+
+				<div className="favorite-container">
+					<button className="toggle-favorites">{svgs.star} Favorites</button>
+				</div>
+
+				<section className="workspaces-actions">
+					<div className="workspaces-wrapper">
+						<div className="workspaces-wrapper">
+							<div>{svgs.workspaces} Workspaces</div>
+							<button>{svgs.threeDots}</button>
+						</div>
+						<button>{svgs.search}</button>
+					</div>
+
+					<div className="workspaces-wrapper">
+						<button className="workspace-list-btn">
+							<div>S {svgs.workspaceHouse}</div>
+							<span>{getUserFirstName()}'s main workspace</span>
+						</button>
+						<button className="add-workspace-item" onClick={onAddBoard}>
+							{svgs.plus}
+						</button>
+					</div>
+				</section>
+
+				<section className="board-links">
+					{boards.map((board) => (
+						<div key={board._id} className="link-wrapper">
+							<NavLink className="board-link" to={`/board/${board._id}`}>
+								{svgs.board}
+								{isEditing && boardToEdit === board._id ? (
+									<InlineEdit
+										value={board.title}
+										onSave={(newTitle) => {
+											onUpdateBoard({ ...board, title: newTitle })
+											setIsEditing(false)
+											setboardToEdit(null)
+										}}
+										isEditing={true}
+									/>
+								) : (
+									<span>{board.title}</span>
+								)}
+							</NavLink>
+							<button
+								className={`board-options ${
+									activeMenuId === board._id ? 'open' : ''
+								}`}
+								onClick={(ev) => toggleContextMenu(ev, board._id)}
+								ref={(el) => (buttonRefs.current[board._id] = el)}
+							>
+								{svgs.threeDots}
+							</button>
+
+							{/* {activeMenuId === board._id && <ContextMenu board={board} onClose={() => setActiveMenuId(null)} onRemoveBoard={onRemoveBoard} onUpdateBoard={onUpdateBoard} onRenameBoard={onRenameBoard} />} */}
+							{activeMenuId === board._id && (
+								<ContextMenu
+									type="board"
+									entity={board}
+									onClose={() => setActiveMenuId(null)}
+									onRemove={onRemoveBoard}
+									onUpdate={onUpdateBoard}
+									onRename={onRenameBoard}
+									referenceElement={buttonRefs.current[board._id]}
+								/>
+							)}
+						</div>
+					))}
+					<NavLink to="/dashboard">
+						{svgs.dashboard} Dashboard and reporting
+					</NavLink>
+				</section>
+			</aside>
+		</>
 	)
 }
